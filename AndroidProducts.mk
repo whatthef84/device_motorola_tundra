04b5a6bf02f8196da914f013de27dcbec2e3c3a1
whatthef84 <64209432+whatthef84@users.noreply.github.com>
2024-02-24 11:42:01 +1000

    Evolutionify AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2023 The AwakenOS Project
+# Copyright (C) 2024 Evolution X
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_tundra.mk
+    $(LOCAL_DIR)/evolution_tundra.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_tundra-user \
-    aosp_tundra-userdebug \
-    aosp_tundra-eng
+    evolution_tundra-user \
+    evolution_tundra-userdebug \
+    evolution_tundra-eng
